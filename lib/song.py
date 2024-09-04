ebd223f6b4a2945c11d03f6a56e92e5b74178ae6
Nobert-Muma <mumanobert17@gmail.com>
2024-09-04 15:28:05 +0300

    Added test cases for the class.

--- lib/song.py
+++ lib/song.py
@@ -37,5 +37,48 @@ class Song:
             cls.artist_count[artist]=1
         else:
             cls.artist_count[artist]+=1
-savage=Song("savage", "21 Savage", "Rap")
-print(savage.artist_count)
+# Creating the first song
+song1 = Song("Savage", "21 Savage", "Rap")
+
+# Print class-level attributes
+print("Count of songs:", Song.count)
+print("Genres:", Song.genres)
+print("Artists:", Song.artists)
+print("Genre count:", Song.genre_count)
+print("Artist count:", Song.artist_count)
+
+# Creating multiple songs
+song2 = Song("Blinding Lights", "The Weeknd", "Pop")
+song3 = Song("Rockstar", "Post Malone", "Rap")
+song4 = Song("Someone Like You", "Adele", "Pop")
+
+# Print class-level attributes
+print("Count of songs:", Song.count)
+print("Genres:", Song.genres)
+print("Artists:", Song.artists)
+print("Genre count:", Song.genre_count)
+print("Artist count:", Song.artist_count)
+
+# Creating songs with duplicate genres and artists
+song5 = Song("Circles", "Post Malone", "Pop")
+song6 = Song("God's Plan", "Drake", "Rap")
+song7 = Song("Hello", "Adele", "Pop")
+
+# Print class-level attributes
+print("Count of songs:", Song.count)
+print("Genres:", Song.genres)
+print("Artists:", Song.artists)
+print("Genre count:", Song.genre_count)
+print("Artist count:", Song.artist_count)
+
+
+# Creating songs with empty or unusual values
+song8 = Song("", "", "")          # Empty values
+song9 = Song("Unique Song", "Unique Artist", "Unique Genre")
+
+# Print class-level attributes
+print("Count of songs:", Song.count)
+print("Genres:", Song.genres)
+print("Artists:", Song.artists)
+print("Genre count:", Song.genre_count)
+print("Artist count:", Song.artist_count)
